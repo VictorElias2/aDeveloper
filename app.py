4e70f4769568c0361b0a091be283ca3235b73f14
VictorElias2 <victorfalciroli966@gmail.com>
2022-02-17 21:50:37 -0300

    New simple feature

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("First app")
+from mimetypes import init
+
+
+def __init__(name, budged):
+    print("Hi {}! How're you? Welcome to this game. Happy fun!".format(name))
+
+__init__("test", 20000)
